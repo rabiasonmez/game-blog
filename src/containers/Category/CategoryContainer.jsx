fe38cbcf981f146e95b3d977ddac87e52dd23896
rabiasonmez <rabiasonmez@gmail.com>
2021-02-18 23:58:04 +0300

    kategori yapısına başlık bölümü eklendi.

--- src/containers/Category/CategoryContainer.jsx
+++ src/containers/Category/CategoryContainer.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Breadcrumb from './../../components/themeLayout/Breadcrumb'
 const CategoryContainer = (props) => {
 
     return (
         <>
+            <Breadcrumb />
             Kategori sayfasının yapısı oluşturulacak
         </>
     )
