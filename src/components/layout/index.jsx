6cfcc8e27714ffe9328cf481ec20ac1eef1f83d8
rabiasonmez <rabiasonmz@gmail.com>
2021-02-19 23:22:46 +0300

    Delete src/components/layout directory

--- src/components/layout/index.jsx
+++
@@ -1,6 +0,0 @@
-import Footer from './Footer';
-import Header from './Header';
-import Breadcrumb from './Breadcrumb';
-import DefaultLayout from './DefaultLayout';
-
-export default { Footer, Header, Breadcrumb, DefaultLayout };
