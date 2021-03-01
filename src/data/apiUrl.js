307458aea38a039d0f0421f950649f508223e411
rabiasonmez <rabiasonmez@gmail.com>
2021-03-01 16:37:48 +0300

    1. ur değiştirildi.

--- src/data/apiUrl.js
+++ src/data/apiUrl.js
@@ -1,4 +1,4 @@
-export const BLOG_IMG_URL = "http://localhost:3000/";
-export const CATEGORY_IMG_URL = "http://localhost:3000/";
-export const COMMENT_IMG_URL = "http://localhost:3000/";
-export const IMG_URL = "http://localhost:3000/";
+export const BLOG_IMG_URL = "https://gamersco.netlify.app/";
+export const CATEGORY_IMG_URL = "https://gamersco.netlify.app/";
+export const COMMENT_IMG_URL = "https://gamersco.netlify.app/";
+export const IMG_URL = "https://gamersco.netlify.app/";
